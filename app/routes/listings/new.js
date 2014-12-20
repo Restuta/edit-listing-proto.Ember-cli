d0f626c888042e46a573e9090d4d7b1418fad460
Restuta <restuta8@gmail.com>
2014-12-19 17:35:06 -0800

    added listing/new route and tests all with "ember generate"

---
+++ app/routes/listings/new.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  redirect: function() {
+    var newDraft = this.store.createRecord('draft');
+
+    var route = this;
+    newDraft.save().then(function() {
+      route.replaceWith('draft', newDraft);
+    });
+  }
+});
