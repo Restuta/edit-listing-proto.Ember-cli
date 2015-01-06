2c9eb1553da991535159ef5215f475a07c556f15
mramasubramanian <mramasubramanian@ingenio.com>
2015-01-06 10:30:26 -0800

    Merging the changes from the old ember project to ember-cli project

---
+++ app/controllers/draft.js
@@ -0,0 +1,52 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+    ourFee: function() {
+        return (this.get('model.minuteRate') * 0.3).toFixed(2);
+    }.property('model.minuteRate'),
+
+    youWillEarn: function() {
+        return (this.get('model.minuteRate') - this.get('ourFee')).toFixed(2);
+    }.property('model.minuteRate'),
+
+    selectedFirstLevelCategoryId: 195,
+    selectedSecondLevelCategoryId: 0,
+    selectedThirdLevelCategoryId: 0,
+
+    firstLevelCategory: function(){
+        var listingCategories = this.get('listingCategories');
+        var firstLevelCategories = listingCategories
+            ? listingCategories.filterBy('parentId', 1) : [];
+
+        return firstLevelCategories;
+    }.property(),
+
+    secondLevelCategory: function(){
+        var listingCategories = this.get('listingCategories');
+        var secondLevelCategories = listingCategories
+            ? listingCategories.filterBy('parentId', this.get('selectedFirstLevelCategoryId')) : [];
+        if (secondLevelCategories.length > 0) {
+            this.set('selectedSecondLevelCategoryId', secondLevelCategories[0].id);
+        }
+
+        return secondLevelCategories;
+    }.property('selectedFirstLevelCategoryId'),
+
+
+    thirdLevelCategory: function(){
+        var listingCategories = this.get('listingCategories');
+        var thirdLevelCategories = listingCategories
+            ? listingCategories.filterBy('parentId', this.get('selectedSecondLevelCategoryId')) : [];
+        if (thirdLevelCategories.length > 0){
+            this.set('selectedThirdLevelCategoryId', thirdLevelCategories[0].id);
+        }
+
+        return thirdLevelCategories;
+    }.property('selectedSecondLevelCategoryId'),
+
+    actions: {
+        save: function() {
+            this.get('model').save();
+        }
+    }
+});
