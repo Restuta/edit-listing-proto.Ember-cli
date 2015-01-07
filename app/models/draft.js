e8e8e26c189799e31cae92dba1a560a7e86750c1
mramasubramanian <mramasubramanian@ingenio.com>
2015-01-07 11:22:58 -0800

    Added additional model properties to support the full draft page

--- app/models/draft.js
+++ app/models/draft.js
@@ -1,6 +1,15 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr(),
-  minuteRate: DS.attr()
+    primaryPhoneNumber: DS.attr(),
+    name: DS.attr(),
+    minuteRate: DS.attr(),
+    categoryId: DS.attr(),
+    chatEnabled: DS.attr(),
+    supportBonus: DS.attr(),
+    title: DS.attr(),
+    profileImage: DS.attr(),
+    summary: DS.attr(),
+    approach: DS.attr(),
+    backgroundInfo: DS.attr()
 });
