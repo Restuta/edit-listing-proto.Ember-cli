a03fa26872930249749eeefcaa22219e86ca0bb7
mramasubramanian <mramasubramanian@ingenio.com>
2015-01-07 15:42:57 -0800

    Adding the missing files for image-upload component

---
+++ app/components/image-upload.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    actions: {
+        showImagePreview: function() {
+            var imagePreview = this.$("#imagePreview");
+            var fileUpload = this.$("#fileUpload");
+            console.log('inside showimagepreview');
+            var files = !!fileUpload[0].files ? fileUpload[0].files : [];
+            if (!files.length || !window.FileReader) {return;} // no file selected, or no FileReader support
+
+            if (/^image/.test( files[0].type)){ // only image file
+                var reader = new FileReader(); // instance of the FileReader
+                reader.onload = function(e){ // set image data as background of div
+                    imagePreview.attr('src', e.target.result);
+                };
+                reader.readAsDataURL(files[0]); // read the local file
+            }
+            else{
+                //Throw error if the type is not image
+            }
+        }
+    }
+});
