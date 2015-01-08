52820faa15240080775bc1e9f9a8767d9ad418fe
mramasubramanian <mramasubramanian@ingenio.com>
2015-01-08 09:14:07 -0800

    Renamed the jquery variables prefixing with $ and fixed the expression to not use !!

--- app/components/image-upload.js
+++ app/components/image-upload.js
@@ -3,16 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     actions: {
         showImagePreview: function() {
-            var imagePreview = this.$("#imagePreview");
-            var fileUpload = this.$("#fileUpload");
-            console.log('inside showimagepreview');
-            var files = !!fileUpload[0].files ? fileUpload[0].files : [];
+            var $imagePreview = this.$("#imagePreview");
+            var $fileUpload = this.$("#fileUpload");
+
+            var files = $fileUpload[0].files ? $fileUpload[0].files : [];
             if (!files.length || !window.FileReader) {return;} // no file selected, or no FileReader support
 
             if (/^image/.test( files[0].type)){ // only image file
                 var reader = new FileReader(); // instance of the FileReader
                 reader.onload = function(e){ // set image data as background of div
-                    imagePreview.attr('src', e.target.result);
+                    $imagePreview.attr('src', e.target.result);
                 };
                 reader.readAsDataURL(files[0]); // read the local file
             }
