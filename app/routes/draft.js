902b4b2bf223b0fa0b698e4500080c07e5256b5f
mramasubramanian <mramasubramanian@ingenio.com>
2015-01-12 15:24:05 -0800

    * Modified draft route to fetch extra meta data for the skills, languages & specializations (needed to be tested)

--- app/routes/draft.js
+++ app/routes/draft.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
     loadListingCategories: Ember.$.getJSON('http://localhost:3008/listingCategories-flat'),
+    loadSpecializations: Ember.$.getJSON('http://localhost:3008/specializations'),
+    loadSkills: Ember.$.getJSON('http://localhost:3008/skills'),
+    loadLanguages: Ember.$.getJSON('http://localhost:3008/languages'),
 
     //todo: fetching of the model by id is a default thing in Ember
     model: function(params) {
@@ -17,26 +20,53 @@ export default Ember.Route.extend({
             listingCategories: self.loadListingCategories.then(function(data) {
                 console.log('loaded listing categories');
                 return data.listingCategories;
+            }),
+            specializationList: self.loadSpecializations.then(function(data) {
+                console.log('loaded specialization list');
+                return data.specializations;
+            }),
+            skillList: self.loadSkills.then(function(data) {
+                console.log('loaded skill list');
+                return data.skills;
+            }),
+            languageList: self.loadLanguages.then(function(data) {
+                console.log('loaded language list');
+                return data.languages;
             })
         };
 
         return Ember.RSVP.hash(promises).then(function(result) {
             result.model.set('listingCategories', result.listingCategories);
+            result.model.set('specializationList', result.specializations);
+            result.model.set('skillList', result.skills);
+            result.model.set('languageList', result.languages);
             return result.model;
         });
     },
 
     ////todo: fetching of the mode land listing categories will be sequential in this case, which delays view rendering
     afterModel: function(draft) {
-        if (draft.get('listingCategories')) {
-            return;
+        var promises = {};
+        var route = this;
+
+        //Load the non-model data here. (e.g) data for dropdown population , list population etc
+        if (!draft.get('listingCategories')) {
+            promises['listingCategories'] = function() {
+                return route.loadListingCategories.then(function (data) {
+                    console.log('loaded listing categories');
+                    return data.listingCategories;
+                });
+            };
         }
 
-        var route = this;
-        return route.loadListingCategories.then(function(response) {
-            route.set('listingCategories', response.listingCategories);
-            draft.set('listingCategories', response.listingCategories);
+        return Ember.RSVP.hash(promises).then(function(result){
+            route.set('listingCategories', result.listingCategories);
         });
+
+        //return route.loadListingCategories.then(function(response) {
+        //    route.set('listingCategories', response.listingCategories);
+        //    draft.set('listingCategories', response.listingCategories);
+        //});
     }
 
     //actions: {
